d4070876167d1214dc33446a031837202ccf0148
sneha-at-hub <snehatmg2004@gmail.com>
2024-08-02 17:40:22 +0545

    User authentication serializer

--- recipesharing/recipeapi/serializers.py
+++ recipesharing/recipeapi/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
 from .models import *
 
+
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+
+
+
+#User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class MyTokenPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['full_name'] = user.profile.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+        token['bio'] = user.profile.bio
+        token['image'] = str(user.profile.image)
+        token['verified'] = user.profile.verified
+        
+        return token
+        
+        
+    
+
+
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
