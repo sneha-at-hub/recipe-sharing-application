b6d5dcc9f12e994ea509153699d18f29a374d606
sneha-at-hub <snehatmg2004@gmail.com>
2024-08-02 17:41:02 +0545

    password validation and token serializer

--- recipesharing/recipeapi/serializers.py
+++ recipesharing/recipeapi/serializers.py
@@ -30,7 +30,35 @@ class MyTokenPairSerializer(TokenObtainPairSerializer):
         
         
     
-
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(
+        write_only=True, validators=[validate_password]
+    )
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'password2']
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({
+                "password": "Passwords do not match"
+            })
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
 
 
 
