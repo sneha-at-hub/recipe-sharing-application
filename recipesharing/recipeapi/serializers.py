150233e4272d7f076167b126e061f0d3a5f38535
sneha-at-hub <snehatmg2004@gmail.com>
2024-08-02 16:55:46 +0545

    added serializers for rating and shopping list:

--- recipesharing/recipeapi/serializers.py
+++ recipesharing/recipeapi/serializers.py
@@ -15,4 +15,13 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-    
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingList
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Rating
+        fields = '__all__'
