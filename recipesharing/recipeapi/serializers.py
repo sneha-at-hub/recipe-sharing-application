f181584ed9db59ad0cf6c5d5fbba59d670bf0ee8
sneha-at-hub <snehatmg2004@gmail.com>
2024-08-02 17:42:48 +0545

    making correction on serializer If the image URL needs to be included, ensure it's correctly formatted

--- recipesharing/recipeapi/serializers.py
+++ recipesharing/recipeapi/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
-from .models import *
-
-
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import *
 
-
-
-
-
-#User Serializer
+# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -23,55 +17,42 @@ class MyTokenPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         token['email'] = user.email
         token['bio'] = user.profile.bio
-        token['image'] = str(user.profile.image)
+        token['image'] = str(user.profile.image.url)  # Include URL if necessary
         token['verified'] = user.profile.verified
-        
         return token
-        
-        
-    
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, validators=[validate_password]
-    )
-    password2 = serializers.CharField(
-        write_only=True, validators=[validate_password]
-    )
-    
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['email', 'username', 'password', 'password2']
-        
+
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({
-                "password": "Passwords do not match"
-            })
+            raise serializers.ValidationError({"password": "Passwords do not match"})
         return attrs
-    
+
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
         )
-        
         user.set_password(validated_data['password'])
         user.save()
-        
         return user
-
-
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = '__all__'
-        
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['category']
-        
+        fields = ['name']
+
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
@@ -84,5 +65,5 @@ class ShoppingListSerializer(serializers.ModelSerializer):
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Rating
+        model = Rating
         fields = '__all__'
