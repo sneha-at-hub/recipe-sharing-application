aa3e24c55000a6f0a707e70bebe2bafbf7515de0
sneha-at-hub <snehatmg2004@gmail.com>
2024-08-25 20:06:17 +0545

    path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,33 @@
-
-import Navbar from './components/Navbar/Navbar'
-import Home from './pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
-import Meals from './pages/Meals/Meals'
-import Popular from './components/Popular/Popular'
-import Footer from './components/Footer/Footer'
-import Login from './components/Login/Login'
-import Detail from './pages/Detail/Detail'
-import Addrecipeshere from './pages/Recipes/Addrecipeshere'
-import Demo from './components/Description/Demo'
-
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Meals from "./pages/Meals/Meals";
+import Popular from "./components/Popular/Popular";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+import Detail from "./pages/Detail/Detail";
+import Addrecipeshere from "./pages/Recipes/Addrecipeshere";
+import Demo from "./components/Description/Demo";
 
 const App = () => {
   return (
     <>
-    
-    
-    <div className='app'>
-      <Navbar />
+      <div className="app">
+        <Navbar />
 
-      <Routes>
-
-      < Route path='/' element={<Home/>} />
-      < Route path='/Meals' element={<Meals/>} />
-      < Route path='/popular' element={<Popular/>} />
-      < Route path='/login' element={<Login/>} />
-      < Route path='/detail' element={<Detail/>} />
-      < Route path='/recipeadd' element={<Addrecipeshere/>} />
-      < Route path='/Demo' element={<Demo/>} />
-
-
- 
-
-      </Routes>
-      
-    </div>
-    <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Meals" element={<Meals />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/recipeadd" element={<Addrecipeshere />} />
+          <Route path="/Demo" element={<Demo />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
