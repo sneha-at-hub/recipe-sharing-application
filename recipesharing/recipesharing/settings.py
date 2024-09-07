f1a7f90dc7bb958bd170e76d5b68852a0ee9a36a
sneha-at-hub <snehatmg2004@gmail.com>
2024-09-07 19:00:51 +0545

    changed css for dropdown profile

--- recipesharing/recipesharing/settings.py
+++ recipesharing/recipesharing/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=50),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
